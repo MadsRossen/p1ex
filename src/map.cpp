c4788a1b5b497cf57cad8ce5197b6d0835391e4d
MadsRossen <mrma19@student.aau.dk>
2019-12-24 12:07:56 +0100

    Node one works

--- src/map.cpp
+++ src/map.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
     //Here we define our map
-    ros::init(argc, argv, "node_one");
+    ros::init(argc, argv, "one");
     ros::NodeHandle nh;
     std::cout<< "hello world" << std::endl;
 
--- src/navigation.cpp
+++ src/navigation.cpp
@@ -1,6 +1,6 @@
 #include <ros/ros.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "two");
   ros::NodeHandle nh;
