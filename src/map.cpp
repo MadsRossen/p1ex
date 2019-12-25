3f905ad9322159ab29edf355d91b1118ce87701c
MadsRossen <mrma19@student.aau.dk>
2019-12-25 21:57:38 +0100

    box is defined

--- src/map.cpp
+++ src/map.cpp
@@ -1,11 +1,77 @@
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <turtlesim/Pose.h>
+#include <iostream>
+#include <turtlesim/SetPen.h>
+#include <turtlesim/TeleportAbsolute.h>
+#include <stdlib.h>
+#include <std_srvs/Empty.h>
+#include <std_msgs/String.h>
+
+using namespace std;
+ void lis(const std_msgs::String::ConstPtr& msg);
 
 int main(int argc, char **argv)
 {
     //Here we define our map
     ros::init(argc, argv, "one");
+    
+
+    float box_size = ros::param::param("~box_size", 9);
+
+
     ros::NodeHandle nh;
-    std::cout<< "hello world" << std::endl;
+    ros::service::waitForService("/turtle1/teleport_absolute", -1);
+    ros::ServiceClient teleport_client = nh.serviceClient<turtlesim::TeleportAbsolute>("/turtle1/teleport_absolute");
+    ros::ServiceClient pen_client = nh.serviceClient<turtlesim::SetPen>("/turtle1/set_pen");
+    ros::Subscriber lengt = nh.subscribe("leng", 10, lis);
+
+    turtlesim::SetPen pen_srv;
+
+    pen_srv.request.off = true;
+    pen_client.call(pen_srv);
+
+    turtlesim::TeleportAbsolute srv;
+
+    srv.request.x = lengt-box_size/2;
+    srv.request.y = 5.5-box_size/2;
+    teleport_client.call(srv);
+
+    pen_srv.request.off = false;
+    pen_srv.request.width = 8;
+    pen_srv.request.r = 228;
+    pen_srv.request.g = 228;
+    pen_client.call(pen_srv);
+
+    srv.request.x = 5.5-box_size/2;
+    srv.request.y = 5.5+box_size/2;
+    teleport_client.call(srv);
+
+    srv.request.x = 5.5+box_size/2;
+    srv.request.y = 5.5+box_size/2;
+    teleport_client.call(srv);
+
+    srv.request.x = 5.5+box_size/2;
+    srv.request.y = 5.5-box_size/2;
+    teleport_client.call(srv);
+
+    srv.request.x = 5.5-box_size/2;
+    srv.request.y = 5.5-box_size/2;
+    teleport_client.call(srv);
+
+    pen_srv.request.off = true;
+    pen_client.call(pen_srv);
+
+    srv.request.x = 5.5;
+    srv.request.y = 5.5;
+    teleport_client.call(srv);
+
+    pen_srv.request.off = false;
+    pen_srv.request.width = 8;
+    pen_srv.request.r = 10;
+    pen_srv.request.g = 228;
+    pen_srv.request.b = 228;
+    pen_client.call(pen_srv);
 
     return 0;
 }
--- src/navigation.cpp
+++ src/navigation.cpp
@@ -1,10 +1,38 @@
+#include <boost/bind.hpp>
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <turtlesim/Pose.h>
+#include <std_srvs/Empty.h>
+#include <iostream>
+#include <turtlesim/TeleportAbsolute.h>
+#include <turtlesim/SetPen.h>
+#include <std_msgs/String.h>
+     
+using namespace std;
+int length;
+int width;
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "two");
   ros::NodeHandle nh;
-  std::cout<< "hello world" << std::endl;
+  ros::Publisher len_pub = nh.advertise<std_msgs::String>("leng", 10);
+  //ros::Subscriber pose_sub = nh.subscribe("turtle1/pose", 1, poseCallback);
+  //ros::Publisher twist_pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
+  //ros::ServiceClient reset = nh.serviceClient<std_srvs::Empty>("reset");
+  //ros::Timer timer = nh.createTimer(ros::Duration(0.016), boost::bind(timerCallback, _1, twist_pub));
 
- return 0;
+  cout << "What is the length ";
+  cin >> length ;
+  cout << "What is the width ";
+  cin >> width ;
+ 
+  std_msgs::String msg;
+  std::stringstream msglen;
+  msglen << width,length << count;
+  msg.data = msglen.str();
+  len_pub.publish(msg);
+
+  return 0;
 }
